9e385d18c3bee5deeb16838d05bafc6f2f8db683
SergiyL <s.lyubchenko@hotmail.com>
2018-04-13 18:54:14 +0300

    add localization

--- localization.js
+++ localization.js
@@ -1,19 +1,36 @@
-var applyLanguage = function (lang) {
-	alert('now language is: ' + lang);
-}
+// var applyLanguage = function (lang) {
+// 	alert('now language is: ' + lang);
+// }
 
 var getCurrentLanguage = function () {
 	var defaultLanguage = 'ua';
-
     return defaultLanguage;
 }
-
-var currentLang = getCurrentLanguage();
-var langEls = document.getElementsByClassName('lang-' + currentLang);
-
-for (var i=0; i<langEls.length; i++) {
-	var langEl = langEls[i];
-	
-    // langEl.style.display = 'inline';
-    langEl.classList.add('visible');
+var radios = document.getElementsByName('lang');
+for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+        localStorage.setItem("language", radios[0].value);
+    }
 }
+
+// var currentLang = location.hash;
+// var langEls = document.getElementsByClassName('lang-' + currentLang);
+
+// //////////////////////////////////////////////////
+
+// var currentLang = getCurrentLanguage();
+// var langEls = document.getElementsByClassName('lang-' + currentLang);
+
+// for (var i=0; i<langEls.length; i++) {
+// 	var langEl = langEls[i];
+	
+// //  langEl.style.display = 'inline';
+//     langEl.classList.add('visible');
+// }
+
+// // $<prefix> == DOMElement
+// var $save = document.querySelector('html body button#save')
+
+// $save.addEventListener('click', function(){
+// 	alert(1);
+// });
